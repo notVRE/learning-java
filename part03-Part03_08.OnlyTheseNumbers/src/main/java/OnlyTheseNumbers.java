27fc7f79cd57764d0f80cea3a21d2da829dcfd02
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-29 14:11:57 +0800

    Part 3-8

--- part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -13,9 +13,14 @@ public class OnlyTheseNumbers {
             if (number == -1) {
                 break;
             }
-
             numbers.add(number);
         }
-
+        System.out.print("From Where? ");
+        int n1 = scanner.nextInt();
+        System.out.print("To where? ");
+        int n2 = scanner.nextInt();
+        for (int i = n1; i <= n2; i++) {
+            System.out.println(numbers.get(i));
+        }
     }
 }
