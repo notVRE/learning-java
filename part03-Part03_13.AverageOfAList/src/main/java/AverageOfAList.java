98324fdd79e37f7c6172878a72492e274e2f1d6a
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-29 14:50:30 +0800

    Part 3-13

--- part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -11,6 +11,20 @@ public class AverageOfAList {
         // adding them on a list until user gives -1.
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+        ArrayList<Double> list = new ArrayList<>();
+        while (true) {
+            Double input = (double) scanner.nextInt();
+            if (input == -1) {
+                break;
+            }
+            list.add(input);
+        }
+        double avg = 0;
+        double sum = 0;
+        for (Double double1 : list) {
+            sum += double1;
+            avg = sum / list.size();
+        }
+        System.out.println("Average: " + avg);
     }
 }
