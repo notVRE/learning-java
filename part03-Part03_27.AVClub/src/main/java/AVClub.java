45cd76b8a22a2952d6ad86adfc9ffa1ce321aaba
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:23:28 +0800

    Part 3-27

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,17 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String av = scanner.nextLine();
+            String [] spc = av.split(" ");
+            for (String string : spc) {
+                if (string.contains("av")) {
+                    System.out.println(string);
+                } 
+            }
+            if (av.isEmpty()) {
+                break;
+            }
+        }
     }
 }
