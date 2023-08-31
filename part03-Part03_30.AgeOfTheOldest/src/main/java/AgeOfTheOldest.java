936e12447d31af867fa22e17b96b5f21eb3cda3e
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:26:49 +0800

    Part 3-30

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,18 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int oage = -1;
+        while (true) {
+            String info = scanner.nextLine();
+            if (info.equals("")) {
+                break;
+            }
+            String [] name = info.split(",");
+           int age = Integer.valueOf(name[1]);
+           if (age > oage) {
+                oage = age;
+           }
+        }
+        System.out.println("Age of the oldest: " + oage);
     }
 }
