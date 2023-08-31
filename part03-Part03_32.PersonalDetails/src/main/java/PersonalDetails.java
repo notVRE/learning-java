ba16a6c14da3c75485bf4d5b14a5f5fb28ee4b42
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:29:20 +0800

    Part 3-32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,25 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int year = 0;
+        String names = "";
+        int ninfo = 0;
 
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String [] info = input.split(",");
+            year = year + Integer.valueOf(info[1]);
+            ninfo = ninfo + 1;
+            for (String name : info) {
+                if (name.length() > names.length()) {
+                    names = name;
+                }
+            }
+        }
+        System.out.println("Longest name: " + names);
+        System.out.println("Average of the birth years: " + (1.0 * year / ninfo));
     }
 }
