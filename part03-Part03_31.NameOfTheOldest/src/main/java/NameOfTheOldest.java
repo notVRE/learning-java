ac07a8067abb2d8de6348372d3f3ab93575e607e
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:28:47 +0800

    Part 3-31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,21 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int oage = -1;
+        String nage = "";
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
 
-
+            String [] info = input.split(",");
+            int age = Integer.valueOf(info[1]);
+            if (age > oage) {
+                oage = age;
+                nage = info[0];
+            }
+        }
+        System.out.println("Name of the oldest: " + nage);
     }
 }
