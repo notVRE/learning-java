920a9851c98fa0e008eefdea7dd83d7e74f3cc50
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:26:09 +0800

    Part 3-29

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,14 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String input = scanner.nextLine();
+            String [] last = input.split(" ");
+            if (input.equals("")) {
+                break;
+            }
+            System.out.println(last[last.length - 1]);
+        }
 
     }
 }
