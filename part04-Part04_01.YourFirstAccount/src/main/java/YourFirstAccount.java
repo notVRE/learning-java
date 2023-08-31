eb985994af67864b4e36bbc8e3286dada2f97359
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-01 00:49:10 +0800

    Part 4-01

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account nwAccount = new Account("BigD", 100.00);
+        nwAccount.deposit(20.0);
+        System.out.println(nwAccount);
+    }
+}
