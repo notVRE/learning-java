d69b8576ef1aed7cf80d272f1f9f11655f5c0d7d
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:18:29 +0800

    Part 3-25

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,21 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String uname1 = "alex";
+        String uname2 = "emma";
+        String pw = "sunshine";
+        String pw1 = "haskell";
+        System.out.print("Enter username: ");
+        String un = String.valueOf(scanner.nextLine());
+        System.out.print("Enter password: ");
+        String pws = String.valueOf(scanner.nextLine());
+        if (un.equals(uname1) && pws.equals(pw)) {
+            System.out.println("You have successfully logged in!");
+        } else if (un.equals(uname2) && pws.equals(pw1)) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+        
     }
 }
