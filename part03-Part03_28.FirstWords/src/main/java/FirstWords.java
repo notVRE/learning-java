ccf3bec590a8d59ae4a85c0377ab6b12d5e6ee28
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-08-31 12:25:30 +0800

    Part 3-28

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,13 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String input = scanner.nextLine();
+            String [] first = input.split(" ");
+            if (input.equals("")) {
+                break;
+            }
+            System.out.println(first[0]);
+        }
     }
 }
