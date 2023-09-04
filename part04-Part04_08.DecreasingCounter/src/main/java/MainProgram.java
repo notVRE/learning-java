b06e1651be0a139f896507b3bab8fb66b292912d
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-04 19:24:57 +0800

    Decreasing Counter

---
+++ part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -0,0 +1,20 @@
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(2);
+
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.reset();
+    }
+}
