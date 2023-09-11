9f405bb933ceaf0836dd99bb5b64d0ba81d9210e
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-11 18:01:21 +0800

    Part 4-25 Printing a specified file

---
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -0,0 +1,20 @@
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingASpecifiedFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String getFile = scanner.nextLine();
+
+        try (Scanner scan = new Scanner(Paths.get(getFile))){
+            while (scan.hasNextLine()) {
+                String row = scan.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
