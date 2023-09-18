78ecb05f791d0ae6a7f61edad08a2bf3cf09e345
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-18 23:00:49 +0800

    Part 5-10 Updated Card payments

--- part05-Part05_10.CardPayments/src/main/java/Main.java
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -2,45 +2,45 @@
 public class Main {
 
     public static void main(String[] args) {
-        // PaymentCard petesCard = new PaymentCard(10);
+        PaymentCard petesCard = new PaymentCard(10);
 
-        // System.out.println("money " + petesCard.balance());
-        // boolean wasSuccessful = petesCard.takeMoney(8);
-        // System.out.println("successfully withdrew: " + wasSuccessful);
-        // System.out.println("money " + petesCard.balance());
+        System.out.println("money " + petesCard.balance());
+        boolean wasSuccessful = petesCard.takeMoney(8);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
 
-        // wasSuccessful = petesCard.takeMoney(4);
-        // System.out.println("successfully withdrew: " + wasSuccessful);
-        // System.out.println("money " + petesCard.balance());
+        wasSuccessful = petesCard.takeMoney(4);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
 
-        // PaymentTerminal unicafeExactum = new PaymentTerminal();
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        // double change = unicafeExactum.eatAffordably(10);
-        // System.out.println("remaining change " + change);
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
 
-        // change = unicafeExactum.eatAffordably(5);
-        // System.out.println("remaining change " + change);
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
 
-        // change = unicafeExactum.eatHeartily(4.3);
-        // System.out.println("remaining change " + change);
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
 
-        // System.out.println(unicafeExactum);
+        System.out.println(unicafeExactum);
 
-        // PaymentTerminal unicafeExactum = new PaymentTerminal();
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        // double change = unicafeExactum.eatAffordably(10);
-        // System.out.println("remaining change: " + change);
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change: " + change);
 
-        // PaymentCard annesCard = new PaymentCard(7);
+        PaymentCard annesCard = new PaymentCard(7);
 
-        // boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        // System.out.println("there was enough money: " + wasSuccessful);
-        // wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        // System.out.println("there was enough money: " + wasSuccessful);
-        // wasSuccessful = unicafeExactum.eatAffordably(annesCard);
-        // System.out.println("there was enough money: " + wasSuccessful);
+        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
 
-        // System.out.println(unicafeExactum);
+        System.out.println(unicafeExactum);
 
         PaymentTerminal unicafeExactum = new PaymentTerminal();
         System.out.println(unicafeExactum);
--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,20 +1,16 @@
 
 public class PaymentTerminal {
 
-    private double money;  // amount of cash
-    private int affordableMeals; // number of sold affordable meals
-    private int heartyMeals;  // number of sold hearty meals
+    private double money; 
+    private int affordableMeals;
+    private int heartyMeals; 
 
 
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
         this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
         if (payment >= 2.5) {
             affordableMeals++;
             this.money += 2.5;
@@ -25,9 +21,6 @@ public class PaymentTerminal {
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
         if (payment >= 4.3) {
             heartyMeals++;
             this.money += 4.3;
@@ -40,7 +33,7 @@ public class PaymentTerminal {
     public boolean eatAffordably(PaymentCard card){
         if (card.balance() >= 2.5) {
             affordableMeals++;
-            card.takeMoney(2.5); // Deduct the meal cost from the card
+            card.takeMoney(2.5); 
             return true;
         } else {
             return false;
@@ -50,7 +43,7 @@ public class PaymentTerminal {
     public boolean eatHeartily(PaymentCard card){
         if (card.balance() >= 4.3) {
             heartyMeals++;
-            card.takeMoney(4.3); // Deduct the meal cost from the card
+            card.takeMoney(4.3);
             return true;
         } else {
             return false;
