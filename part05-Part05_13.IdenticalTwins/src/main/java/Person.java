8d2ab0656544d339a1019ea72bf7a804914ef677
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-19 22:59:19 +0800

    Part 5-13 Identical twins

---
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -0,0 +1,41 @@
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private SimpleDate birthday;
+    private int height;
+    private int weight;
+
+    public Person(String name, SimpleDate birthday, int height, int weight) {
+        this.name = name;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // implement an equals method here for checking the equality of objects
+
+    public boolean equals(Object compared){
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person comparePerson = (Person) compared;
+
+        if (this.name.equals(comparePerson.name) && 
+        this.birthday.equals(comparePerson.birthday) &&
+        this.height == comparePerson.height &&
+        this.weight == comparePerson.weight) {
+            return true;
+        }
+
+
+        return false;
+    }
+}
