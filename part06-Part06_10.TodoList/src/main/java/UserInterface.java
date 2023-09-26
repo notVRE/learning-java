f699b999285a90947eee69e265770e2f3b4848bf
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-09-26 17:35:52 +0800

    Part 6-10 TodoList

--- part06-Part06_10.TodoList/src/main/java/UserInterface.java
+++ part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class UserInterface {
-    
+    private TodoList list;
+    private Scanner scanner;
+
+    public UserInterface(TodoList todoList,Scanner scanner){
+        this.list = todoList;
+        this.scanner = scanner;
+    }
+
+    public void start(){
+        while (true) {
+            System.out.print("Command: ");
+            String opt = scanner.nextLine();
+
+            if (opt.equals("stop")) {
+                break;
+            } 
+
+            if (opt.equals("add")){
+                System.out.print("To add: ");
+                String oa = scanner.nextLine();
+                list.add(oa); 
+            } 
+            
+            if (opt.equals("list")) {
+                list.print();
+            } 
+            
+            if (opt.equals("remove")){
+                System.out.print("Which one is removed? ");
+                int rem = Integer.valueOf(scanner.nextLine());
+                list.remove(rem);
+            }
+        }
+    }
 }
